80a3f62a50c6d2c95ee125ece0fd17ba4a72c400
Jacob Winther <jacob.winther@maersk.com>
2025-05-02 10:02:17 +0200

    Added both backends as options to the frontend service layer so there is no favorite

--- frontend/src/services/VendorService.ts
+++ frontend/src/services/VendorService.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import type { Vendor } from '../types/Vendor';
+const useNodeBackend = true; // Set to true if using Java backend
+const API_URL_JAVA = 'http://localhost:3001/api'; // Assuming backend runs on port 3000
+const API_URL_NODE = 'http://localhost:3000/api';
 
-const API_URL = 'http://localhost:8081/api'; // Assuming backend runs on port 3000
-
+const API_URL = useNodeBackend ? API_URL_NODE : API_URL_JAVA;
 export const VendorService = {
   async getVendors(): Promise<Vendor[]> {
     try {
